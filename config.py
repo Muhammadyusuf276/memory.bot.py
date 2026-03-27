9a77eddaf4bb4c05061f18856eea7ff7b4941132
Muhammadyusuf276 <ybegimqulov01@gmail.com>
2026-03-27 14:28:12 +0500

    new

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
-ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "Yusu1F_m1").replace("@", "")
+ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "Yusu1F_m2").replace("@", "")
 
 _db_ch = os.getenv("DB_CHANNEL_ID", "")
 DB_CHANNEL_ID = int(_db_ch) if _db_ch else None
